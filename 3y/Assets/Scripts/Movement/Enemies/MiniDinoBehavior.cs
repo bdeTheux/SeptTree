64f5b200d368036de1db37ddb0d683dd316c7c1a
bdeTheux <boris.detheux@student.vinci.be>
2022-03-27 15:24:06 +0200

    Add - MiniDino collide

--- 3y/Assets/Scripts/Movement/Enemies/MiniDinoBehavior.cs
+++ 3y/Assets/Scripts/Movement/Enemies/MiniDinoBehavior.cs
@@ -78,7 +78,6 @@ public class MiniDinoBehavior : MonoBehaviour
         if (playerMask == (playerMask | 1 << other.gameObject.layer))
         {
             ChooseType(other);
-            
         }
     }
 
